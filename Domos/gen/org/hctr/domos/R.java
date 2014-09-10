11f3fe395b6512e155d735c3fc29663483d937ff
Hector <Hector@192.168.0.51>
2014-09-10 17:33:49 +0200

    2 subida

--- Domos/gen/org/hctr/domos/R.java
+++ Domos/gen/org/hctr/domos/R.java
@@ -37,27 +37,32 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f090004;
-        public static final int container=0x7f090001;
-        public static final int drawer_layout=0x7f090000;
+        public static final int container=0x7f090000;
+        public static final int drawer_layout=0x7f090001;
         public static final int navigation_drawer=0x7f090002;
         public static final int section_label=0x7f090003;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int fragment_main=0x7f030001;
-        public static final int fragment_navigation_drawer=0x7f030002;
+        public static final int activity_luces=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int fragment_luces=0x7f030002;
+        public static final int fragment_main=0x7f030003;
+        public static final int fragment_navigation_drawer=0x7f030004;
     }
     public static final class menu {
         public static final int global=0x7f080000;
-        public static final int main=0x7f080001;
+        public static final int luces=0x7f080001;
+        public static final int main=0x7f080002;
     }
     public static final class string {
         public static final int action_example=0x7f050006;
         public static final int action_settings=0x7f050007;
         public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050009;
         public static final int navigation_drawer_close=0x7f050005;
         public static final int navigation_drawer_open=0x7f050004;
         public static final int title_RaspiShark=0x7f050001;
+        public static final int title_activity_luces=0x7f050008;
         public static final int title_luces=0x7f050002;
         public static final int title_rejillas=0x7f050003;
     }
